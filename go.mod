beb1198244b5bf635d50713b5cae5453311de127
xpetit <32063953+xpetit@users.noreply.github.com>
2022-05-21 14:51:20 +0200

    Add MultiLines

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/xpetit/x
 
 go 1.18
 
-require golang.org/x/exp v0.0.0-20220328175248-053ad81199eb
+require golang.org/x/exp v0.0.0-20220518171630-0b5c67f07fdf
--- x.go
+++ x.go
@@ -1,6 +1,10 @@
 package x
 
-import "golang.org/x/exp/constraints"
+import (
+	"strings"
+
+	"golang.org/x/exp/constraints"
+)
 
 // Check panics if one of its arguments is a non-nil error.
 func Check(a ...any) {
@@ -44,4 +48,41 @@ func Max[T constraints.Integer](x, y T) T {
 		return x
 	}
 	return y
+}
+
+// MultiLines formats a multiline raw string, changing:
+// 	`
+// 		First line
+// 			Second line
+// 			Third line
+// 	`
+// to:
+// 	`First line
+// 		Second line
+// 		Third line`
+// It is intended to be called like this:
+// 	MultiLines(`
+// 		First Line
+// 			Second line
+// 			Third line
+// 	`)
+func MultiLines(s string) string {
+	lines := strings.Split(s, "\n")
+	if len(lines) < 3 {
+		panic("FmtLines: expected raw string enclosed with new lines")
+	}
+	lines = lines[1 : len(lines)-1]
+	padding := 0
+loop:
+	for ; padding < len(lines[0]); padding++ {
+		switch lines[0][padding] {
+		case '\t', '\n', '\v', '\f', '\r', ' ':
+		default:
+			break loop
+		}
+	}
+	for i, line := range lines {
+		lines[i] = line[Min(padding, len(line)):]
+	}
+	return strings.Join(lines, "\n")
 }
