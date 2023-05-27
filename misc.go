6c0d75279338d6705210fd42366ac30f9c660e06
xpetit <32063953+xpetit@users.noreply.github.com>
2023-05-27 15:22:13 +0200

    Add Goroutines function

--- misc.go
+++ misc.go
@@ -1,6 +1,21 @@
 package x
 
+import "sync"
+
 // Ptr returns a pointer to v.
 func Ptr[T any](v T) *T {
 	return &v
+}
+
+// Goroutines spawns nb goroutines executing fn. It returns a function that waits for them to finish.
+func Goroutines(nb int, fn func()) (wait func()) {
+	var wg sync.WaitGroup
+	for i := 0; i < nb; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fn()
+		}()
+	}
+	return wg.Wait
 }
