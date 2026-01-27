759eef1be3f6da2a407ccb92900859e892859cd1
xpetit <32063953+xpetit@users.noreply.github.com>
2026-01-27 13:41:13 +0100

    Remove no longer needed function (we now have (*WaitGroup) Go(func()))

--- misc.go
+++ misc.go
@@ -1,22 +1,6 @@
 package x
 
-import "sync"
-
 // Ptr returns a pointer to v.
 func Ptr[T any](v T) *T {
 	return &v
-}
-
-// Goroutines spawns nb goroutines executing fn. It returns a function that waits for them to finish.
-func Goroutines(nb int, fn func(i int)) (wait func()) {
-	var wg sync.WaitGroup
-	for i := 0; i < nb; i++ {
-		i := i
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			fn(i)
-		}()
-	}
-	return wg.Wait
 }
